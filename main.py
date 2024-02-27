9fe8e224f164cb01076c85e9930bf591c66c91da
bobthebeef <bob.leboeuf89@gmail.com>
2024-02-26 22:53:43 -0500

    setup and main changes

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+# Main driver for calling the alphavantage intraday stock market API: https://www.alphavantage.co/documentation/
+# Ultimate goal is to create an app that will accept input from users and display output data for the specific stock selected
+# 
+#
 import requests
+import json
+
+def CallAPI(ticker,interval,key):
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ticker+'&interval='+interval+'&apikey='+key
+    print(url) 
+    response = requests.get(url)
+    return response.json()
+
+def HandleResponse():
+    pass
 
 def main():
-    pass
+    #TODO: replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    response = CallAPI("IBM","5min","demo")
     # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-    #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-    #r = requests.get(url)
-    #data = r.json()
+    print(response)
+    pass
+
+if __name__ == '__main__':
+    main()
